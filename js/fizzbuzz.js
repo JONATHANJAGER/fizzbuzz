9cf4c6882501ece94ffb0636ad7df6256eae569e
jonathanjager <jagerbill11@gmail.com>
2016-07-01 22:10:27 -0400

    modified html and js

---
+++ js/fizzbuzz.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var i;
+
+	$('#numberForm').submit(function(e){
+		e.preventDefault();
+		console.log('here');
+		i = $('.number').value();
+	})
+
+
+	for (i=1; i<=100; i++) {
+		if (i % 3 === 0) {
+			if(i % 5 === 0 ) {
+				document.write('FizzBuzz' + '<br>');
+			}  else{
+				document.write('Fizz' + '<br>');
+			}
+
+		}
+	}
+	else if(i % 5 === 0){
+		document.write('Buzz' + '<br>');
+	} else {
+		console.log.(i);
+	}
+
+});
